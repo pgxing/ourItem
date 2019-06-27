1b638863796995321e26e80945de1fb5cf40a6e3
yinxiang <yinxiangis@163.com>
2019-06-27 19:05:58 +0800

    'yin'

--- ourapp/src/main.js
+++ ourapp/src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import MintUi from 'mint-ui';
 
+Vue.use(MintUi)
 Vue.config.productionTip = false
-
+import 'mint-ui/lib/style.css'
 new Vue({
   router,
   render: h => h(App)
---
+++ ourapp/src/pages/home.vue
@@ -0,0 +1,49 @@
+<template>
+    <section class = 'home'>
+        sdsfghjk
+        <div class = 'test'>
+            <MBanner></MBanner>
+
+        </div>
+    </section>
+</template>
+
+<script>
+
+import MBanner from '../components/m-banner.vue'
+
+
+export default {
+    data(){
+        return{}
+    },
+    
+   components:{
+       MBanner
+   },
+   
+}
+</script>
+
+<style lang = 'less' scope>
+    @import '../common/style/index.less';
+    .home{
+        width:100%;
+        position:fixed;
+        .top(88);
+        bottom:0;
+        overflow:hidden;
+        .test{
+            height:2000px;
+            width:100%;
+            background:pink;
+
+        }
+    }
+
+
+</style>
+
+
+
+
