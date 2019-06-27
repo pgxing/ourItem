89d8cae25db7d97353b1ea07d85e334e3246116c
pgx <pgxing1101@163.com>
2019-06-27 17:22:58 +0800

    1.0

--- ourapp/src/router.js
+++ ourapp/src/router.js
@@ -3,8 +3,29 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
+import Home from 'pages/home'
+import Car from 'pages/car'
+import My from 'pages/my'
+import Serch from 'pages/serch'
+
 export default new Router({
   routes: [
-    
+      {
+        path:'/home',
+        component:Home
+      },
+      {
+        path:'/car',
+        component:Car
+      },
+      {
+        path:'/my',
+        component:My
+      },
+      {
+        path:'/serch',
+        component:Serch
+      }
+
   ]
 })
