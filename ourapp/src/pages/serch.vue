b651cc67819c51226f6526e1dc5cd4ddf2d6f0c5
pgx <pgxing1101@163.com>
2019-06-27 21:26:33 +0800

    pgx

--- ourapp/src/pages/serch.vue
+++ ourapp/src/pages/serch.vue
@@ -3,17 +3,47 @@
         <header>
             <div class="serInp">♥ 搜索</div>
         </header>
+        <ul class="aside">
+            <li v-for="(item,index) in info"
+                :key="index"
+                @click="chose(index)"
+                :class="index == state ? 'green' : ''"
+            >{{item.title}}{{index}}</li>
+        </ul>
     </div>
 </template>
 <script>
 export default {
-    
+    data(){
+        return{
+            info:[],
+            state:0
+        }
+    },
+    methods:{
+        initData(){
+            this.$axios.get('https://www.easy-mock.com/mock/5d14b08fea4d3e1eb5fb9725/shuju')
+            .then((res) => {
+                // let list = res.list;
+                // this.singerDate(list)
+                console.log(res.data.info)
+                this.info = res.data.info
+            })
+        },
+        chose(index){
+            console.log(index)
+            this.state = index;
+        }
+    },
+    mounted(){
+        this.initData();
+    }
 }
 </script>
 <style lang="less" scoped>
 @import '~style/index.less'; 
 .serch{
-    height: 1000px;
+    height: 100%;
     .w(375);
     background: orange;
     header{
@@ -31,6 +61,21 @@ export default {
             .f_s(14);
             border-radius: 15px;
             
+        }
+    }
+    .aside{
+        height: 100%;
+        .w(90);
+        background: #f7f7f8;
+        li{
+            .f_s(14);
+            .h(48);
+            .l_h(48);
+            text-align: center;
+        }
+        .green{
+            color: #99bb42;
+            background: #fff;
         }
     }
 }
